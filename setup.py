16362b7cd47d8cbbd702e4a91bed4adf2fc992a5
Christopher Armstrong <radix@twistedmatrix.com>
2015-06-08 00:40:51 -0500

    prep 0.9 of txeffect

--- setup.py
+++ setup.py
@@ -3,14 +3,13 @@ import setuptools
 
 setuptools.setup(
     name="txeffect",
-    version="0.1a2",
+    version="0.9",
     description="Effect/Twisted integration",
     long_description=open('README.rst').read(),
-    url="http://github.com/python-effect/txeffect/",
+    url="https://github.com/python-effect/txeffect/",
     author="Christopher Armstrong",
     license="MIT",
     classifiers=[
-        'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
